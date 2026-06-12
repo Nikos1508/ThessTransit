6d41c17843b73f4976e190afed80815e15326e79
Nikos Tsilas <tsilasnikos@gmail.com>
2026-06-12 17:59:11 +0300

    Creation of the bottom bar and color theme

--- app/src/main/java/com/example/thesstransit/ui/theme/Theme.kt
+++ app/src/main/java/com/example/thesstransit/ui/theme/Theme.kt
@@ -10,35 +10,42 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import android.view.View
+import androidx.compose.runtime.SideEffect
+import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+private val darkScheme = darkColorScheme(
+    primary = primaryDark,
+    secondary = secondaryDark,
+    tertiary = tertiaryDark,
+    background = backgroundDark,
+    surface = surfaceDark,
+    onPrimary = onPrimaryDark,
+    onSecondary = onSecondaryDark,
+    onTertiary = onTertiaryDark,
+    onBackground = onBackgroundDark,
+    onSurface = onSecondaryDark,
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+private val lightScheme = lightColorScheme(
+    primary = primaryLight,
+    secondary = secondaryLight,
+    tertiary = tertiaryLight,
+    background = backgroundLight,
+    surface = surfaceLight,
+    onPrimary = onPrimaryLight,
+    onSecondary = onSecondaryLight,
+    onTertiary = onTertiaryLight,
+    onBackground = onBackgroundLight,
+    onSurface = onSecondaryLight,
 )
 
 @Composable
 fun ThessTransitTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    content: @Composable() () -> Unit
 ) {
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
@@ -46,8 +53,14 @@ fun ThessTransitTheme(
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        darkTheme -> darkScheme
+        else -> lightScheme
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            setUpEdgeToEdge(view, darkTheme)
+        }
     }
 
     MaterialTheme(
@@ -55,4 +68,15 @@ fun ThessTransitTheme(
         typography = Typography,
         content = content
     )
+}
+
+private fun setUpEdgeToEdge(view: View, darkTheme: Boolean) {
+    val window = (view.context as Activity).window
+
+    WindowCompat.setDecorFitsSystemWindows(window, false)
+
+    val controller = WindowCompat.getInsetsController(window, view)
+
+    controller.isAppearanceLightStatusBars = !darkTheme
+    controller.isAppearanceLightNavigationBars = !darkTheme
 }
