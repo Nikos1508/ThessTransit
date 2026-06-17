b0181fcb14f892bfe04b85332513919fbdc87114
Nikos Tsilas <tsilasnikos@gmail.com>
2026-06-17 13:11:18 +0300

    Trying to make api work (no luck)

--- app/src/main/java/com/example/thesstransit/ui/item/RoutesScreen.kt
+++ app/src/main/java/com/example/thesstransit/ui/item/RoutesScreen.kt
@@ -1,23 +1,58 @@
 package com.example.thesstransit.ui.item
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import xyz.teogramm.oasth.Oasth
+import xyz.teogramm.oasth.OasthData
+import xyz.teogramm.oasth.base.BusMasterLine
 
 @Composable
 fun RoutesScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
+    var oasthData by remember { mutableStateOf<OasthData?>(null) }
+    var isLoading by remember { mutableStateOf(true) }
 
-        Text(
-            text = "Routes Screen",
-            color = MaterialTheme.colorScheme.onBackground
-        )
+    LaunchedEffect(Unit) {
+        withContext(Dispatchers.IO) {
+            try {
+                val fetchedData = Oasth().fetchData()
+
+                withContext(Dispatchers.Main) {
+                    oasthData = fetchedData
+                    isLoading = false
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                withContext(Dispatchers.Main) {
+                    isLoading = false
+                }
+            }
+        }
+    }
+
+    if (isLoading) {
+        Text(text = "Φόρτωση γραμμών...")
+    } else {
+
+        val allMasterLines: Map<Int, BusMasterLine> = oasthData?.masterLines ?: emptyMap()
+
+        println(allMasterLines)
+
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(allMasterLines.values.toList()) { masterLine ->
+                Text(text = "${masterLine.number}: ${masterLine.nameEL}")
+            }
+        }
+
     }
 }
