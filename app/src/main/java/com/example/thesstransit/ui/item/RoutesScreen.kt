7d10e74e6973ecc3c6b49b9651357cb935ee0658
Nikos Tsilas <tsilasnikos@gmail.com>
2026-07-13 16:35:49 +0300

    New Settings screen

--- app/src/main/java/com/example/thesstransit/ui/item/RoutesScreen.kt
+++ app/src/main/java/com/example/thesstransit/ui/item/RoutesScreen.kt
@@ -1,6 +1,5 @@
 package com.example.thesstransit.ui.item
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -18,7 +17,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
@@ -27,12 +25,15 @@ import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.text.font.FontWeight
@@ -43,14 +44,10 @@ import com.example.thesstransit.ui.components.BusRouteRowItem
 import com.example.thesstransit.ui.components.ScreenHeader
 import com.example.thesstransit.ui.utils.groupRoutes
 import com.example.thesstransit.ui.viewModels.FavoritesViewModel
+import com.example.thesstransit.ui.viewModels.LanguageViewModel
 import com.example.thesstransit.ui.viewModels.RoutesViewModel
 import io.gitlab.mitsiosm.oseth.data.Route
 import kotlinx.coroutines.launch
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import com.example.thesstransit.ui.viewModels.LanguageViewModel
-import io.gitlab.mitsiosm.oseth.data.Language
 
 @Composable
 fun RoutesScreen(
@@ -190,22 +187,6 @@ fun RoutesScreen(
                 onProfileClick = onBackClick,
                 modifier = Modifier.weight(1f)
             )
-
-            IconButton(
-                onClick = {
-                    languageViewModel.toggleLanguage()
-                    Log.d("LANG", "clicked")
-                }
-            ) {
-                Text(
-                    text =
-                        if (language == Language.GREEK)
-                            "GR"
-                        else
-                            "EN",
-                    fontSize = 28.sp
-                )
-            }
         }
 
         Box(
--- app/src/main/java/com/example/thesstransit/ui/item/SettingsScreen.kt
+++ app/src/main/java/com/example/thesstransit/ui/item/SettingsScreen.kt
@@ -1,2 +1,136 @@
 package com.example.thesstransit.ui.item
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Language
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Palette
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.thesstransit.ui.components.ScreenHeader
+import com.example.thesstransit.ui.viewModels.LanguageViewModel
+import io.gitlab.mitsiosm.oseth.data.Language
+
+@Composable
+fun SettingsScreen(
+    onBackClick: () -> Unit,
+    viewModel: LanguageViewModel = viewModel()
+) {
+
+    val language by viewModel.language.collectAsState()
+
+    Column( modifier = Modifier.fillMaxSize() ) {
+        ScreenHeader(
+            title = "Ρυθμίσεις",
+            onBackClick = onBackClick,
+            onProfileClick = {}
+        )
+
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(2.dp)
+        ) {
+            item {
+                ListItem(
+                    headlineContent = {
+                        Text("Γλώσσα")
+                    },
+
+                    supportingContent = {
+                        Text(
+                            if (language == Language.GREEK)
+                                "Ελληνικά"
+                            else
+                                "English"
+                        )
+                    },
+
+                    leadingContent = {
+                        Icon(
+                            Icons.Default.Language,
+                            null
+                        )
+                    },
+
+                    trailingContent = {
+                        Switch(
+                            checked = language == Language.ENGLISH,
+                            onCheckedChange = {
+                                viewModel.toggleLanguage()
+                            }
+                        )
+                    }
+                )
+                HorizontalDivider()
+            }
+            item {
+                ListItem(
+                    headlineContent = {
+                        Text("Θέμα")
+                    },
+
+                    supportingContent = {
+                        Text("Σύντομα διαθέσιμο")
+                    },
+
+                    leadingContent = {
+                        Icon(Icons.Default.Palette, null)
+                    }
+                )
+                HorizontalDivider()
+            }
+
+            item {
+                ListItem(
+                    headlineContent = {
+                        Text("Ειδοποιήσεις")
+                    },
+
+                    supportingContent = {
+                        Text("Σύντομα διαθέσιμο")
+                    },
+
+                    leadingContent = {
+                        Icon(
+                            Icons.Default.Notifications,
+                            null
+                        )
+                    }
+                )
+                HorizontalDivider()
+            }
+
+            item {
+                ListItem(
+                    headlineContent = {
+                        Text("Σχετικά με την εφαρμογή")
+                    },
+
+                    supportingContent = {
+                        Text("Έκδοση 0.8")
+                    },
+
+                    leadingContent = {
+                        Icon(
+                            Icons.Default.Info,
+                            null
+                        )
+                    }
+                )
+            }
+        }
+    }
+}
